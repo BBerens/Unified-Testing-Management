90517028d7c13f81d49987485e53eda4c34f8ae5
bberens <brett.s.berens@gmail.com>
2017-05-02 15:54:08 -0700

    Initial Commit

---
+++ TestDB/Table1.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Table1]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(100) NULL, 
+    [Baseline] NCHAR(100) NULL, 
+    [Labels] NCHAR(100) NULL, 
+    [Date Created] DATETIME NULL, 
+    [Date Modified] DATETIME NULL 
+)
